6da513b11c8ec25e5a2bc7a0e4e13e342cb5a0f4
Jaíne Rosa <jaine.rosa_11@live.com>
2024-08-06 22:16:40 -0300

    :construction: feat(categoria): Criacao dos componentes

--- frontend/store-front/src/app/app.module.ts
+++ frontend/store-front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriaFormComponent } from './componentes/categoria/categoria-form/categoria-form.component';
+import { CategoriaDetalheComponent } from './componentes/categoria/categoria-detalhe/categoria-detalhe.component';
+import { ProdutoListaComponent } from './componentes/produto/produto-lista/produto-lista.component';
+import { ProdutoFormComponent } from './componentes/produto/produto-form/produto-form.component';
+import { ProdutoDetalheComponent } from './componentes/produto/produto-detalhe/produto-detalhe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoListaComponent,
+    ProdutoFormComponent,
+    ProdutoDetalheComponent,
+    
   ],
   imports: [
     BrowserModule,
