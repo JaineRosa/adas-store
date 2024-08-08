2692a340780c8f4f4bba0e1f0887a5b3f8f32e13
Jaíne Rosa <jaine.rosa_11@live.com>
2024-08-08 20:00:01 -0300

    :construction: feat(categoria): organizando o componente lista

--- backend/store/src/main/java/com/adas/store/controller/CategoriaController.java
+++ backend/store/src/main/java/com/adas/store/controller/CategoriaController.java
@@ -42,4 +42,6 @@ public class CategoriaController {
         return ResponseEntity.ok(categoria);
     }
 
+
+
 }
--- frontend/store-front/src/app/app.module.ts
+++ frontend/store-front/src/app/app.module.ts
@@ -8,19 +8,31 @@ import { CategoriaDetalheComponent } from './componentes/categoria/categoria-det
 import { ProdutoListaComponent } from './componentes/produto/produto-lista/produto-lista.component';
 import { ProdutoFormComponent } from './componentes/produto/produto-form/produto-form.component';
 import { ProdutoDetalheComponent } from './componentes/produto/produto-detalhe/produto-detalhe.component';
+import { CabecalhoComponent } from './componentes/compartilhados/cabecalho/cabecalho.component';
+import { MenuComponent } from './componentes/compartilhados/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CategoriaListaComponent } from './componentes/categoria/categoria-lista/categoria-lista.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CategoriaDetalheComponent,
+    CategoriaFormComponent,
+    CategoriaListaComponent,
     ProdutoListaComponent,
     ProdutoFormComponent,
     ProdutoDetalheComponent,
+    CabecalhoComponent,
+    MenuComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
