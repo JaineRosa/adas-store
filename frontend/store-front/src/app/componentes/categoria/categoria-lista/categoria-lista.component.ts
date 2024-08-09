ccd872b5d13d3f5097de9d102f2d33135f989ba0
Jaíne Rosa <jaine.rosa_11@live.com>
2024-08-08 22:17:44 -0300

    :construction: feat(categoria): finalizando layout

--- frontend/store-front/src/app/componentes/categoria/categoria-lista/categoria-lista.component.ts
+++ frontend/store-front/src/app/componentes/categoria/categoria-lista/categoria-lista.component.ts
@@ -14,7 +14,29 @@ export class CategoriaListaComponent {
   constructor(private categoriaService: CategoriaService){}
 
   ngOnInit(): void{
-    this.carregarCategorias();
+    //this.carregarCategorias();
+    this.categorias = [
+      {
+        id : 1,
+        nome: "Software",
+        status: false
+      },
+      {
+        id : 2,
+        nome: "IA",
+        status: true
+      },
+      {
+        id : 3,
+        nome: "Periféricos",
+        status: false
+      },
+      {
+        id : 4,
+        nome: "Novidades",
+        status: false
+      }
+    ]
   }
 
   carregarCategorias(): void {
